487dbc536631336100791e28c6bb320a35ba5b94
fur866 <fur866@gmail.com>
2016-04-03 12:44:59 +1000

    Part 1c: The GameModel Initialiser/Constructor

--- app/src/main/java/com/example/fahad/csci342_assignment1/GameModel.java
+++ app/src/main/java/com/example/fahad/csci342_assignment1/GameModel.java
@@ -1,5 +1,7 @@
 package com.example.fahad.csci342_assignment1;
 
+import android.graphics.drawable.Drawable;
+
 import java.util.ArrayList;
 
 /**
@@ -14,7 +16,34 @@ public class GameModel {
     public gameInterface interfaceReference;
     private int gameScore;
 
+    //constructor
+    GameModel(int numberOfTiles, ArrayList<Drawable> images){
 
+        //check if the number of tiles are even or not. If not we simply make it even
+        if(numberOfTiles % 2 != 0){
+            numberOfTiles += 1;
+        }
+
+        //get some data
+        int totalImages = images.size();
+        int imageCounter = 0;
+        this.tiles = new ArrayList<TileData>();
+
+        for(int i = 0; i < numberOfTiles; i++){
+            //add a TileData : the identifier varies from 1 to total images
+            this.tiles.add(new TileData(images.get(imageCounter),imageCounter+1));
+
+            //if we have run at the end of images, set the imageCounter to 1 again
+            imageCounter = imageCounter+1 >= totalImages ? 1 : imageCounter+1;
+        }
+
+        //set all other members to default
+        this.firstTappedTile = 0;
+        this.secondTappedTile = 0;
+        this.isFirst = true;
+        this.totalMatchedTiles = 0;
+        this.gameScore = 0;
+    }
     public interface gameInterface
     {
 

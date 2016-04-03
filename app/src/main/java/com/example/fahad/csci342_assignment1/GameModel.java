5f8301e80e6a81b57c8515f6c8a599ca406ff35e
fur866 <fur866@gmail.com>
2016-04-03 12:19:09 +1000

    Part1b - The model class has been set up. All the properties in the class have been added to the class.

---
+++ app/src/main/java/com/example/fahad/csci342_assignment1/GameModel.java
@@ -0,0 +1,22 @@
+package com.example.fahad.csci342_assignment1;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Fahad on 3/04/2016.
+ */
+public class GameModel {
+    private int firstTappedTile;
+    private int secondTappedTile;
+    private ArrayList<TileData> tiles;
+    private Boolean isFirst;
+    private int totalMatchedTiles;
+    public gameInterface interfaceReference;
+    private int gameScore;
+
+
+    public interface gameInterface
+    {
+
+    }
+}

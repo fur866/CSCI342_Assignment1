26b52b85934e6427672ba631b0f95e5aa21ba8c4
fur866 <fur866@gmail.com>
2016-04-03 11:57:03 +1000

    Part1a - TileData class has been adeed.

---
+++ app/src/main/java/com/example/fahad/csci342_assignment1/TileData.java
@@ -0,0 +1,37 @@
+package com.example.fahad.csci342_assignment1;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Fahad on 3/04/2016.
+ */
+public class TileData {
+    private Drawable Image;
+    private int identifier;
+
+
+    TileData(Drawable image, int id)
+    {
+        //check if image is an instance of drawable
+        if(image != null) {
+            this.Image = image;
+        }
+
+        //check if id is an int or not
+        if(id == (int)id) {
+            this.identifier = id;
+        }
+    }
+
+    //get function for Image
+    public Drawable getImage()
+    {
+        return this.Image;
+    }
+
+    //get function for drawable
+    public int getIdentifier()
+    {
+        return this.identifier;
+    }
+}

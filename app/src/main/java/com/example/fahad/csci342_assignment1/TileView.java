588e1a36d7b4355b9975ec9c070d033ccf81888c
fur866 <fur866@gmail.com>
2016-04-03 19:04:30 +1000

    Part 2 - TileView class added and linked against Tile Layouts in acitivty_main.xml file. Part 2 ends here.

---
+++ app/src/main/java/com/example/fahad/csci342_assignment1/TileView.java
@@ -0,0 +1,16 @@
+package com.example.fahad.csci342_assignment1;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by Fahad on 3/04/2016.
+ */
+public class TileView extends LinearLayout{
+
+    public TileView(Context context, AttributeSet attr)
+    {
+        super(context,attr);
+    }
+}

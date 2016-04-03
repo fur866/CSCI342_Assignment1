1641cb1a93952b20864cd523f1a4044aee731a79
fur866 <fur866@gmail.com>
2016-04-03 13:54:40 +1000

    Part 1f: The description / toString method - Some testing was done.

--- app/src/main/java/com/example/fahad/csci342_assignment1/MainActivity.java
+++ app/src/main/java/com/example/fahad/csci342_assignment1/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.fahad.csci342_assignment1;
 
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ArrayList<Drawable> pictures = new ArrayList<Drawable>();
+        pictures.add(ContextCompat.getDrawable(this,R.drawable.baldhill));
+        pictures.add(ContextCompat.getDrawable(this,R.drawable.cathedral));
+        pictures.add(ContextCompat.getDrawable(this,R.drawable.question));
+
+        GameModel model = new GameModel(6,pictures);
+        Log.d("Here: ",model.description());
     }
 }

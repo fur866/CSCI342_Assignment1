9298149b9b847ff3b103f523a33c3a21af33082e
fur866 <fur866@gmail.com>
2016-04-04 19:56:18 +1000

    Part 3b - The default backrgound has been added to each tile. lso colors that were added to the parent linearlayouts have been removed.

--- app/src/main/java/com/example/fahad/csci342_assignment1/TileView.java
+++ app/src/main/java/com/example/fahad/csci342_assignment1/TileView.java
@@ -2,7 +2,9 @@ package com.example.fahad.csci342_assignment1;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -20,5 +22,13 @@ public class TileView extends LinearLayout{
     public TileView(Context context, AttributeSet attr)
     {
         super(context,attr);
+
+        this.imageView = new ImageView(context);
+        this.imageView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+        this.image = ContextCompat.getDrawable(context,R.drawable.question);
+
+        this.imageView.setImageDrawable(this.image);
+
+        this.addView(this.imageView);
     }
 }

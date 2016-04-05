e573bfd200e9dcc64796f85a5bb1251b3c31003c
fur866 <fur866@gmail.com>
2016-04-05 19:39:15 +1000

    Part 3c & 3d - This has been completed and tested.

--- app/src/main/java/com/example/fahad/csci342_assignment1/TileView.java
+++ app/src/main/java/com/example/fahad/csci342_assignment1/TileView.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -15,20 +17,62 @@ public class TileView extends LinearLayout{
 
     private Drawable image;
     private ImageView imageView;
-    private GameModel.gameInterface tileInterface;
+    private TileViewListener tileListener;
     private int tileIndex;
+    private Boolean isFlipped;
 
 
     public TileView(Context context, AttributeSet attr)
     {
         super(context,attr);
+        this.isFlipped = false;
 
         this.imageView = new ImageView(context);
         this.imageView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
         this.image = ContextCompat.getDrawable(context,R.drawable.question);
 
         this.imageView.setImageDrawable(this.image);
+        this.imageView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                if(!isFlipped) {
+                    revealImage();
+                }
+                else {
+                    coverImage();
+                }
+                //tileListener.didSelectTile(TileView.this);
+            }
+        });
 
         this.addView(this.imageView);
+    }
+
+
+    public void revealImage()
+    {
+        if(!isFlipped) {
+            this.image = ContextCompat.getDrawable(getContext(),R.drawable.lake);
+            this.imageView.setImageDrawable(this.image);
+            this.isFlipped = true;
+        }
+    }
+
+    public void coverImage()
+    {
+        if(isFlipped) {
+            this.imageView.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.question));
+            this.isFlipped = false;
+        }
+    }
+
+    public void hideImage()
+    {
+        this.imageView.setImageResource(android.R.color.transparent);
+    }
+
+    public interface TileViewListener {
+        void didSelectTile(TileView tileView);
     }
 }
